2aca2c6bdb8a783b731d95c5ff8a3d7bebc38477
Lovebin Joseph Robin <98700499+lovebin123@users.noreply.github.com>
2024-06-07 15:29:53 +0530

    added progress

--- frontend/src/pages/Facegallary.jsx
+++ frontend/src/pages/Facegallary.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Flex, useDisclosure, Button, Image, Avatar, Stack } from '@chakra-ui/react';
+import { Flex, useDisclosure, Button, Image, Avatar, Stack, CircularProgress } from '@chakra-ui/react';
 import axios from 'axios';
 import {
   Modal,
@@ -9,11 +9,12 @@ import {
   ModalBody,
   ModalCloseButton,
 } from '@chakra-ui/react';
-import { Grid, GridItem } from '@chakra-ui/react'
+
 function Facegallary() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [keys, setKeys] = useState([]);
   const [selectedImages, setSelectedImages] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
@@ -22,6 +23,8 @@ function Facegallary() {
         setKeys(response.data);
       } catch (error) {
         console.log(error);
+      } finally {
+        setLoading(false);
       }
     }
     fetchData();
@@ -32,6 +35,7 @@ function Facegallary() {
     txt.innerHTML = html;
     return txt.value;
   };
+
   const handleAvatarClick = (images) => {
     const imageURLs = images
       .replace(/[\[\]']/g, '') 
@@ -44,9 +48,9 @@ function Facegallary() {
         const uppercaseJpegIndex = trimmedURL.indexOf(".JPEG");
         const firebaseIndex = trimmedURL.indexOf("https://kkhsobiinyhntegdenee.supabase.co/");
         const startIndex = firebaseIndex !== -1 ? firebaseIndex : 0;
-  
+
         let endIndex = -1;
-  
+
         // Determine the end index for .jpg or .JPG
         if (lowercaseJpgIndex !== -1) {
           endIndex = lowercaseJpgIndex + 4;
@@ -59,15 +63,15 @@ function Facegallary() {
         } else if (uppercaseJpegIndex !== -1) {
           endIndex = uppercaseJpegIndex + 5;
         }
-  
+
         let modifiedURL = endIndex !== -1 ? trimmedURL.substring(startIndex, endIndex) : trimmedURL.substring(startIndex);
-  
+
         // Remove %22 after the image extension (.jpg, .JPG, .jpeg, .JPEG) at the end of the URL
         modifiedURL = modifiedURL.replace(/\.jpg%22$/, ".jpg")
                                 .replace(/\.JPG%22$/, ".JPG")
                                 .replace(/\.jpeg%22$/, ".jpeg")
                                 .replace(/\.JPEG%22$/, ".JPEG");
-  
+
         return modifiedURL;
       })
       .map(url => url.replace("http://localhost:5174/dash/%22", ""));
@@ -76,32 +80,35 @@ function Facegallary() {
     onOpen();
   };
 
-
   return (
     <Flex justifyContent="center" gap={10} alignItems="center" flexWrap={'wrap'} pt={8}>
-      {keys.map((item, index) => (
-        <Flex key={index} cursor="pointer" onClick={() => handleAvatarClick(item.values)} pl={10}>
-        <Avatar src={item.key} size={'2xl'} borderColor={'#00ADB5'} borderWidth={4} objectFit={'cover'} />
-        </Flex>
-      ))}
-      <Modal isOpen={isOpen} onClose={onClose} size={'4xl'}   >
-  <ModalOverlay />
-  <ModalContent background={'#3A4750'} >
-    <ModalCloseButton color={'#EEEEEE'} />
-    <ModalBody>
-      <Flex flexWrap="wrap" gap={10} flexShrink={1} >
-        {selectedImages.map((image, index) => (
-          <Image margin={"0 auto"} position={'relative'} top={4}   borderRadius={12} key={index} src={image.trim()} w={'18vw'}    />
-        ))}
-      </Flex>
-    </ModalBody>
-    <ModalFooter>
-      <Button colorScheme="blue" mr={3} onClick={onClose}>
-        Close
-      </Button>
-    </ModalFooter>
-  </ModalContent>
-</Modal>
+      {loading ? (
+        <CircularProgress isIndeterminate color='#00ADB5' />
+      ) : (
+        keys.map((item, index) => (
+          <Flex key={index} cursor="pointer" onClick={() => handleAvatarClick(item.values)} pl={10}>
+            <Avatar src={item.key} size={'2xl'} borderColor={'#00ADB5'} borderWidth={4} objectFit={'cover'} />
+          </Flex>
+        ))
+      )}
+      <Modal isOpen={isOpen} onClose={onClose} size={'4xl'} >
+        <ModalOverlay />
+        <ModalContent background={'#3A4750'}>
+          <ModalCloseButton color={'#EEEEEE'} />
+          <ModalBody>
+            <Flex flexWrap="wrap" gap={10} flexShrink={1}>
+              {selectedImages.map((image, index) => (
+                <Image margin={"0 auto"} position={'relative'} top={4} borderRadius={12} key={index} src={image.trim()} w={'18vw'} />
+              ))}
+            </Flex>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 }
