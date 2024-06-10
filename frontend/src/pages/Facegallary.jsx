48c98be92227c7e51482e5ffad0ac1c7ea5af025
Lovebin Joseph Robin <98700499+lovebin123@users.noreply.github.com>
2024-06-10 19:28:19 +0530

    uii

--- frontend/src/pages/Facegallary.jsx
+++ frontend/src/pages/Facegallary.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Flex, useDisclosure, Button, Image, Avatar, Stack, CircularProgress } from '@chakra-ui/react';
 import axios from 'axios';
-import { Gallery } from "react-grid-gallery";
-
 import {
   Modal,
   ModalOverlay,
